63fadd9b736a72f88e684d142602b3cd1e3f2b17
Qingqi Shi <qingqishi@live.com>
2015-07-16 20:34:27 +0800

    Add meta tags for compatibility

--- common/view/header.php
+++ common/view/header.php
@@ -3,6 +3,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
+
     <title><?php echo $title; ?></title>
 
     <!-- Bootstrap -->
