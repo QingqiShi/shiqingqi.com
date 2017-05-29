843148886fc31aa484cecb1ed855c255e83184ab
Qingqi Shi <qingqishi@live.com>
2017-05-29 02:59:58 +0100

    Added web componenets

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(sassMiddleware({
     dest: path.join(__dirname, 'public'),
     sourceMap: false
 }));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
