4029ce5aa3de8bf8778fb686c699c21367e2f050
Qingqi Shi <qingqishi@live.com>
2017-06-08 23:35:49 +0100

    Added server push feature (sort of)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
+var pusher = require('./pusher');
 var router = express.Router();
+
+var pushList = [
+    {
+        path: '/webcomponentsjs/webcomponents-loader.js',
+        type: 'javascript'
+    },
+    {
+        path: '/elements/main.html',
+        notBower: true
+    },
+    {
+        path: '/polymer/polymer-element.html',
+    },
+    {
+        path: '/iron-icons/iron-icons.html',
+    },
+    {
+        path: '/iron-pages/iron-pages.html',
+    },
+    {
+        path: '/iron-media-query/iron-media-query.html'
+    },
+    {
+        path: '/iron-location/iron-location.html'
+    },
+    {
+        path: '/iron-location/iron-query-params.html'
+    },
+    {
+        path: '/app-layout/app-scroll-effects/app-scroll-effects.html'
+    },
+    {
+        path: '/paper-icon-button/paper-icon-button.html'
+    },
+    {
+        path: '/app-layout/app-layout.html'
+    },
+    {
+        path: '/paper-tabs/paper-tabs.html'
+    },
+    {
+        path: '/paper-item/paper-item.html'
+    },
+    {
+        path: '/paper-listbox/paper-listbox.html'
+    },
+    {
+        path: '/elements/my-footer.html',
+        notBower: true
+    }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Qingqi Shi' });
+    pusher(res, pushList);
+
+    res.render('index', { title: 'Qingqi Shi' });
+
 });
 
 module.exports = router;
---
+++ routes/pusher.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var fs = require('fs');
+
+// Obj = [{path, type}]
+
+var htmlOptions = {
+    method: 'GET',
+    request: { accept: '*/*' },
+    response: { 'content-type': 'text/html; charset=UTF-8' }
+}
+
+var javascriptOptions = {
+    method: 'GET',
+    request: { accept: '*/*' },
+    response: { 'content-type': 'application/javascript' }
+}
+
+var cssOptions = {
+    method: 'GET',
+    request: { accept: '*/*' },
+    response: { 'content-type': 'text/css' }
+}
+
+var pusher = function(res, options) {
+
+
+    for (var i = 0; i < options.length; i++)
+    {
+        var obj = options[i];
+
+        // Content
+        if (typeof obj.notBower != 'undefined' && obj.notBower) {
+            var content = fs.readFileSync(__dirname + '/../public/' + obj.path);
+        } else {
+            var content = fs.readFileSync(__dirname + '/../bower_components/' + obj.path);
+        }
+
+        // Options
+        if (typeof obj.type == 'undefined' || obj.type == 'html') {
+            var opt = htmlOptions;
+        } else if (obj.type == 'javascript') {
+            var opt = javascriptOptions;
+        } else {
+            var opt = cssOptions;
+        }
+
+        // Push
+        res.push(obj.path, opt).end(content);
+    }
+};
+
+module.exports = pusher;
