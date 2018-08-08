afefc73c6f174ab71057e0b21122f11036c2778b
Qingqi Shi <qingqishi@live.com>
2018-08-08 15:07:10 +0100

    Add service worker

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+var ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+var path = require('path');
+
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    configureWebpack: {
+        plugins: [
+            new ServiceWorkerWebpackPlugin({
+                entry: path.join(__dirname, 'src/sw.js')
+            })
+        ]
+    }
 };
