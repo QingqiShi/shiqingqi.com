17150e6b93ae1f5ca1e2834383387580ea4cfd35
Qingqi Shi <qingqishi@live.com>
2018-08-26 19:44:34 +0100

    Redesign homepage banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,147 +1,258 @@
 <template>
     <div class="banner">
-        <div class="wrapper">
-            <round-img :src="require('../assets/selfie.jpg')" class="selfie" />
-            <card class="intro">
-                <t t="name" tag="h1" class="title" />
-                <p class="tags">
-                    <t t="coder" class="palette-1" />
-                    <t t="husband" class="palette-3" />
-                    <t t="gamer" class="palette-2" />
-                    <t t="frontEndDev" class="palette-4" />
-                    <t t="designer" class="palette-3" />
-                    <t t="chinese" class="palette-5" />
-                    <t t="photographer" class="palette-2" />
-                    <t t="creator" class="palette-1" />
-                </p>
-            </card>
+        <div class="title">
+            <transition name="text-reveal" appear>
+                <t t="name" tag="h1" />
+            </transition>
+            <transition name="slide-down" appear>
+                <div class="background"><t t="name" tag="h1" /></div>
+            </transition>
         </div>
+        <div class="selfie-container">
+            <transition name="slide-left" appear>
+                <img :src="require('../assets/selfie.jpg')" alt="selfie" class="selfie">
+            </transition>
+        </div>
+        
+        <transition name="slide-right" appear>
+            <div class="tags">
+                <t t="coder" class="tag" />
+                <t t="husband" class="tag" />
+                <t t="gamer" class="tag" />
+                <t t="frontEndDev" class="tag" />
+                <t t="designer" class="tag" />
+                <t t="chinese" class="tag" />
+                <t t="photographer" class="tag" />
+                <t t="creator" class="tag" />
+            </div>
+        </transition>
+
+        <transition name="fade-in" appear>
+            <div class="shape shape1"></div>
+        </transition>
+        
+        <transition name="fade-in" appear>
+            <div class="shape shape2"></div>
+        </transition>
+            
+        <transition name="fade-in" appear>
+            <div class="shape shape3"></div>
+        </transition>
     </div>
 </template>
 
 <script>
-import Card from '@/components/Card.vue';
-import RoundImg from '@/components/RoundImg.vue';
-
 export default {
-    name: 'Banner',
-    components: {
-        Card,
-        RoundImg
-    }
+    name: 'Banner'
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../scssUtils/breakpoint';
 @import '../scssUtils/colors';
+@import '../scssUtils/shadow';
+@import '../scssUtils/transitions';
 
-$selfie-size: 24vw;
-
-h1 {
-    margin-bottom: 0;
+@keyframes gradient {
+    0% {
+        background-position: 0% 0%;
+    }
+    50% {
+        background-position: 100% 100%;
+    }
+    100% {
+        background-position: 0% 0%;
+    }
 }
 
 .banner {
-    background-image: url(../assets/main-bg.jpg);
-    background-position: center;
-    background-size: cover;
-    text-align: center;
-}
-
-.wrapper {
-    padding: 0 2rem 1rem;
-
-    @include breakpoint($tablet) {
-        padding: 0 5rem 3rem;
-    }
-
-    @include breakpoint($laptop) {
-        padding: 8rem 2rem 8rem;
-        display: grid;
-        grid-template-columns: 23rem minmax(auto, max-content);
-        align-items: center;
-        justify-content: center;
-        vertical-align: middle;
-        max-width: $laptop;
-        margin: 0 auto;
-    }
-
-    @include breakpoint($desktop) {
-        padding: 8rem 5rem 8rem;
-        grid-template-columns: 32rem minmax(auto, max-content);
-        max-width: $desktop;
-        margin: 0 auto;
-    }
-}
-
-.selfie {
     position: relative;
-    top: $selfie-size / 2;
-    width: $selfie-size;
-    margin: 0 auto;
-    z-index: 10;
+    display: grid;
+    grid-template-columns: 20% 80%;
+    background: linear-gradient(135deg, $red, $purple, $blue);
+    background-size: 300% 300%;
+    overflow: hidden;
+    animation: gradient 25s ease infinite;
 
     @include breakpoint($laptop) {
-        position: static;
-        width: 23rem;
-    }
-
-    @include breakpoint($desktop) {
-        width: 32rem;
-    }
-}
-
-.intro {
-    position: relative;
-    padding: ($selfie-size / 2) 1rem ($selfie-size / 3);
-
-    @include breakpoint($laptop) {
-        position: static;
-        padding: 1rem 2rem 2rem;
-        margin-left: 2rem;
-        max-width: 56rem;
-        max-height: 23rem;
-        justify-self: start;
-    }
-
-    @include breakpoint($desktop) {
-        max-height: 32rem;
+        grid-template-columns: 50% 50%;
     }
 }
 
 .title {
-    font-size: 3rem;
+    color: $white;
+    grid-column: 1 / 3;
 
-    @include breakpoint($tablet) {
-        font-size: 5rem;
+    h1 {
+        position: relative;
+        top: 0.8rem;
+        margin: 0;
+        padding-left: 2rem;
+        text-align: left;
+        font-size: 2rem;
+        z-index: 10;
+
+        @include breakpoint($laptop) {
+            font-size: 3.5rem;
+        }
+
+        @include breakpoint($desktop) {
+            font-size: 5rem;
+        }
     }
 
-    @include breakpoint($desktop) {
-        font-size: 8rem;
+    .background {
+        position: absolute;
+        top: 0;
+        left: 3rem;
+        color: transparent;
+        background-color: $grey;
+        user-select: none;
+        pointer-events: none;
+        z-index: 5;
+
+        h1 {
+            padding: 0;
+        }
+    }
+}
+
+.selfie-container {
+    padding: 3rem 0;
+    grid-row: 2 / 3;
+    grid-column: 2 / 3;
+
+    @include breakpoint($laptop) {
+        padding-bottom: 6rem;
+    }
+
+    .selfie {
+        position: relative;
+        width: 100%;
+        overflow: hidden;
+        border-top-left-radius: 0.5rem;
+        border-bottom-left-radius: 0.5rem;
+        user-select: none;
+        z-index: 10;
+
+        @include medium-shadow;
+
+        @include breakpoint($tablet) {
+            @include large-shadow;
+        }
     }
 }
 
 .tags {
-    font-size: 1rem;
+    position: relative;
+    top: -5rem;
+    margin-right: 20%;
+    padding: 2rem;
+    grid-row: 3 / 4;
+    grid-column: 1 / 3;
+    color: $grey;
+    background-color: $white;
+    border-top-right-radius: 0.5rem;
+    border-bottom-right-radius: 0.5rem;
+    text-align: left;
+    z-index: 20;
+
+    @include medium-shadow;
 
     @include breakpoint($tablet) {
-        font-size: 1.5rem;
+        @include large-shadow;
+    }
+
+    @include breakpoint($laptop) {
+        position: static;
+        grid-row: 2 / 3;
+        grid-column: 1 / 2;
+        align-self: center;
+        padding: 5rem;
+        margin: 0;
+        color: $white;
+        background-color: transparent;
+        box-shadow: none;
     }
 
     @include breakpoint($desktop) {
-        font-size: 2.5rem;
+        padding: 8rem;
     }
 
-    span {
-        margin: 0.2rem;
-        display: inline-block;
-    }
+    .tag {
+        display: block;
+        margin-right: 10%;
+        text-transform: uppercase;
+        font-size: 1.5rem;
+        text-align: right;
 
-    @for $i from 1 through 5 {
-        .palette-#{$i} {
-            color: nth($palette, $i);
+        &:after {
+            content: '.';
+        }
+
+        @include breakpoint($tablet) {
+            display: inline-block;
+            text-align: left;
+            margin-right: 1rem;
+        }
+
+        @include breakpoint($laptop) {
+            display: block;
+            text-align: right;
+            font-size: 2rem;
+        }
+
+        @include breakpoint($desktop) {
+            font-size: 3rem;
         }
     }
+}
+
+.shape {
+    position: absolute;
+    z-index: 0;
+}
+
+.shape1 {
+    left: -10%;
+    top: 10rem;
+    width: 50%;
+    padding-top: 50%;
+    border-radius: 50%;
+    background: linear-gradient(
+        45deg,
+        RGBA($blue-rgb, 0),
+        RGBA($blue-rgb, 0) 50%,
+        RGBA($blue-rgb, 0.5)
+    );
+}
+
+.shape2 {
+    right: -10%;
+    top: -11rem;
+    width: 50%;
+    height: 15rem;
+    border-radius: 2rem;
+    background: linear-gradient(
+        -135deg,
+        RGBA($blue-rgb, 0),
+        RGBA($blue-rgb, 0) 50%,
+        RGBA($blue-rgb, 0.5)
+    );
+    transform: skewX(-20deg);
+}
+
+.shape3 {
+    bottom: 5%;
+    right: 10%;
+    width: 30%;
+    padding-top: 30%;
+    border-radius: 50%;
+    background: linear-gradient(
+        -135deg,
+        RGBA($blue-rgb, 0),
+        RGBA($blue-rgb, 0) 50%,
+        RGBA($blue-rgb, 0.5)
+    );
 }
 </style>
