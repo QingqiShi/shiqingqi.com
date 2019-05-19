9569220cd1b1563f8fc3156f1befb1890fd3f157
Qingqi Shi <qingqishi@live.com>
2019-05-19 21:41:11 +0100

    Update work place to WTC

--- src/components/WorksSection.vue
+++ src/components/WorksSection.vue
@@ -1,49 +1,56 @@
 <template>
-    <flat-section>
-        <t t="works" tag="h1" />
+      <flat-section>
+            <t t="works" tag="h1" />
         <div class="current">
-            <!-- <img class="logo" src="@/assets/Salmon_Logo.svg" alt="Salmon Ltd"> -->
+                  <!-- <img class="logo" src="@/assets/Salmon_Logo.svg" alt="Salmon Ltd"> -->
             <div class="description">
-                <t t="myCurrentJob" tag="span" class="tag heading-font" />
+                        <t t="myCurrentJob" tag="span" class="tag heading-font" />
                 <t t="salmonDescription" tag="p" />
                 <t t="salmonWork" tag="p" />
             </div>
-            <intersect-transition name="slide-right">
-                <card class="business-card">
+                  <intersect-transition name="slide-right">
+                        <card class="business-card">
                     <div class="logo">
-                        <img src="@/assets/Salmon_Logo_Grey.svg" alt="Salmon Ltd">
-                        <svg class="diagonalBottom" 
-                            viewBox="0 0 1280 100" 
-                            preserveAspectRatio="none" 
+                        <img
+                            src="@/assets/wunderman_thompson_commerce_logo.svg"
+                                          alt="Salmon Ltd"
+                        >
+                        <svg
+                            class="diagonalBottom"
+                            viewBox="0 0 1280 100"
+                                          preserveAspectRatio="none"
                             role="presentation"
-                            aria-hidden="true">
+                            aria-hidden="true"
+                        >
                             <polygon points="0 0 1280 100 0 100" fill="#07393C" />
                         </svg>
-                        <svg class="diagonalLeft" 
-                            viewBox="0 0 200 1280" 
-                            preserveAspectRatio="none" 
+                        <svg
+                                          class="diagonalLeft"
+                                          viewBox="0 0 200 1280"
+                            preserveAspectRatio="none"
                             role="presentation"
-                            aria-hidden="true">
-                            <polygon points="0 0 200 0 0 1280" fill="#07393C" />
-                        </svg>
+                            aria-hidden="true"
+                        >
+                                          <polygon points="0 0 200 0 0 1280" fill="#07393C" />
+                                    </svg>
                     </div>
-                    <div class="info">
+                              <div class="info">
                         <t t="salmonJobTitle" tag="em" class="heading-font" />
                         <t t="name" tag="div" />
-                        <div>qshi@salmon.com</div>
-                        <div>
+                                    <div>qshi@salmon.com</div>
+                                    <div>
                             <span>Salmon Ltd</span>
                             <span>64 Clarendon Road</span>
                             <span>Watford</span>
-                            <span>Hertfordshire, WD17 1DA</span>
-                        </div>
+                                          <span>Hertfordshire, WD17 1DA</span>
+                                    </div>
                     </div>
                 </card>
             </intersect-transition>
-        </div>
+            </div>
 
         <!-- <t t="noExperienceExcuse" tag="p" class="excuse heading-font" /> -->
-    </flat-section>
+      </flat-section>
 </template>
 
 <script>
@@ -73,6 +80,7 @@ export default {
         position: relative;
         padding-bottom: 2rem;
         margin: 0;
+        text-align: right;
     }
 
     .info {
@@ -95,7 +103,9 @@ export default {
     }
 
     img {
-        margin: 2rem 5% 0 50%;
+        margin: 2rem 0 0;
+        padding: 0 5%;
+        width: 50%;
     }
 
     em {
@@ -146,7 +156,8 @@ export default {
 
     img {
         width: 80%;
-        margin: 0 2rem 2rem 0;
+        margin: 0 1rem 2rem 0;
+        padding: 0;
         align-self: flex-end;
     }
 }
