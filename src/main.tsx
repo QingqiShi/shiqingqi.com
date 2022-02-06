16f0201c12b4ee57e372259b5b73e64dc1090eae
Qingqi Shi <hi@qingqi.dev>
2022-02-06 17:58:16 +0000

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,32 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { cononicalOrigin } from './utils/pathname';
+
+(async () => {
+  if (window.location.hostname === cononicalOrigin) {
+    const { initializeApp } = await import('firebase/app');
+    const { getPerformance } = await import('firebase/performance');
+    const { getAnalytics, isSupported, setAnalyticsCollectionEnabled } =
+      await import('firebase/analytics');
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCCNVad_-hk_vjG2uAGNUfTY8KeiwlK7wE',
+      authDomain: 'shiqingqi-27cab.firebaseapp.com',
+      databaseURL: 'https://shiqingqi-27cab.firebaseio.com',
+      projectId: 'shiqingqi-27cab',
+      storageBucket: 'shiqingqi-27cab.appspot.com',
+      messagingSenderId: '207610618557',
+      appId: '1:207610618557:web:b3609648132bb22543cdd4',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    getPerformance(app);
+    getAnalytics(app);
+  }
+})();
 
 ReactDOM.render(
   <StrictMode>
