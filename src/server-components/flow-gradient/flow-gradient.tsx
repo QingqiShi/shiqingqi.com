e4a57b4488b667ed0d4fbe5ec812ea08ef2cfece
Qingqi Shi <hi@qingqi.dev>
2024-01-21 07:03:16 +0000

    Attempt to fix ISR

--- src/server-components/flow-gradient/flow-gradient.tsx
+++ src/server-components/flow-gradient/flow-gradient.tsx
@@ -1,16 +1,13 @@
-import { promises as fs } from "fs";
 import { FlowGradientClient } from "./flow-gradient-client";
 
 export async function FlowGradient() {
   const [fragShader, vertShader] = await Promise.all([
-    fs.readFile(
-      process.cwd() + "/src/server-components/flow-gradient/shaders/fs.glsl",
-      "utf-8"
-    ),
-    fs.readFile(
-      process.cwd() + "/src/server-components/flow-gradient/shaders/vs.glsl",
-      "utf-8"
-    ),
+    fetch(
+      "https://0tius9gdxi32io0t.public.blob.vercel-storage.com/shaders/fs.glsl"
+    ).then((result) => result.text()),
+    fetch(
+      "https://0tius9gdxi32io0t.public.blob.vercel-storage.com/shaders/vs.glsl"
+    ).then((result) => result.text()),
   ]);
   return <FlowGradientClient fs={fragShader} vs={vertShader} />;
 }
