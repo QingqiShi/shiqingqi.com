4c363a8e4539d0aeee46ac828d1311319262bde2
Qingqi Shi <hi@qingqi.dev>
2024-01-22 01:03:01 +0000

    Update theme color on theme update

--- src/utils/theme-hack.ts
+++ src/utils/theme-hack.ts
@@ -7,12 +7,19 @@ const systemClassName = getDocumentClassName(null);
 export const themeHack = `(function(){
     try{
         const theme = localStorage.getItem("theme");
+        const meta = document.createElement("meta");
+        meta.setAttribute("name", "theme-color");
         if (theme === "dark") {
             document.documentElement.className = "${darkClassName}";
+            meta.setAttribute("content", "#292929");
         } else if (theme === "light") {
             document.documentElement.className = "${lightClassName}";
+            meta.setAttribute("content", "#f3eded");
         } else {
             document.documentElement.className = "${systemClassName}";
+            const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+            meta.setAttribute("content", isDark ? "#292929" : "#f3eded");
         }
+        document.head.appendChild(meta);
     }catch(t){}
 })();`;
