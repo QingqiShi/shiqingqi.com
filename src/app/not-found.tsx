373edda7dca0b1e009e89f593f63e5cbd74ec5af
Qingqi Shi <hi@qingqi.dev>
2025-01-04 01:44:03 +0000

    fix: not found page layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import Layout from "./[locale]/layout";
+import Layout from "./[locale]/(home)/layout";
+import RootLayout from "./[locale]/layout";
 
 export const metadata: Metadata = {
   title: "404 | Qingqi Shi",
@@ -8,9 +9,11 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <Layout params={Promise.resolve({ locale: "en" })}>
-      <h1>404</h1>
-      <p>Page not found 😢</p>
-    </Layout>
+    <RootLayout params={Promise.resolve({ locale: "en" })}>
+      <Layout params={Promise.resolve({ locale: "en" })}>
+        <h1>404</h1>
+        <p>Page not found 😢</p>
+      </Layout>
+    </RootLayout>
   );
 }
