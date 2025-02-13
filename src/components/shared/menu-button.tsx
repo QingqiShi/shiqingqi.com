0ed4a1c1374e2079a42739d4d304d3186d372cb7
Qingqi Shi <hi@qingqi.dev>
2025-02-13 23:07:21 +0000

    fix: menu animation details

--- src/components/shared/menu-button.tsx
+++ src/components/shared/menu-button.tsx
@@ -42,7 +42,20 @@ export function MenuButton({
 
   const outsideClicked = useRef(false);
   const containerRef = useClickAway<HTMLDivElement>(() => {
-    if (isMenuShown) {
+    // Check if there are animations happening, if so prevent closing the menu
+    // https://www.bram.us/2025/01/01/view-transitions-snippets-getting-all-animations-linked-to-a-view-transition/
+    const vtAnimations = document.getAnimations().filter((animation) => {
+      const animationEffect = animation.effect as {
+        target: HTMLElement;
+        pseudoElement?: string;
+      } | null;
+      return (
+        animationEffect?.target === document.documentElement &&
+        animationEffect.pseudoElement?.startsWith("::view-transition")
+      );
+    });
+
+    if (isMenuShown && vtAnimations.length === 0) {
       setIsMenuShown(false);
       outsideClicked.current = true;
     }
@@ -94,7 +107,7 @@ export function MenuButton({
         onBlur={(e) => {
           if (
             isMenuShown &&
-            !containerRef.current?.contains(e.relatedTarget) &&
+            !containerRef.current?.contains(e.currentTarget) &&
             !outsideClicked.current
           ) {
             setIsMenuShown(false);
