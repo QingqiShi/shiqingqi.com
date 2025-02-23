e1f807697978423344feff4ed9bd27e3803b89cc
Qingqi Shi <hi@qingqi.dev>
2025-02-23 17:53:01 +0000

    feat: add ratings to movie posters (#1058)

--- src/components/movie-database/movie-card.tsx
+++ src/components/movie-database/movie-card.tsx
@@ -1,8 +1,9 @@
 import * as stylex from "@stylexjs/stylex";
 import { Card } from "@/components/shared/card";
 import { useTranslations } from "@/hooks/use-translations";
-import { color, font, layer, ratio } from "@/tokens.stylex";
+import { border, color, font, layer, ratio, space } from "@/tokens.stylex";
 import type { MovieListItem } from "@/utils/tmdb-api";
+import { useTranslationContext } from "@/utils/translation-context";
 import { PosterImage } from "./poster-image";
 import type translations from "./poster-image.translations.json";
 
@@ -12,6 +13,8 @@ interface MovieCardProps {
 
 export function MovieCard({ movie }: MovieCardProps) {
   const { t } = useTranslations<typeof translations>("posterImage");
+  const { locale } = useTranslationContext();
+  const formatter = new Intl.NumberFormat(locale, { maximumFractionDigits: 1 });
 
   return (
     <Card
@@ -29,8 +32,10 @@ export function MovieCard({ movie }: MovieCardProps) {
             <div css={styles.errorText}>{t("failedToLoadImage")}</div>
           </div>
         )}
+        {movie.rating ? (
+          <div css={styles.rating}>{formatter.format(movie.rating)}</div>
+        ) : null}
       </div>
-      {/* TODO: Render ratings */}
     </Card>
   );
 }
@@ -66,5 +71,21 @@ const styles = stylex.create({
   errorText: {
     fontSize: font.size_00,
     color: color.textMuted,
+  },
+  rating: {
+    position: "absolute",
+    top: space._1,
+    left: space._1,
+    width: space._5,
+    height: space._5,
+    borderRadius: border.radius_round,
+    backgroundColor: color.backgroundRaised,
+    borderWidth: ".2em",
+    borderColor: color.textMain,
+    borderStyle: "solid",
+    fontSize: font.size_00,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
