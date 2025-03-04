eb6cb3fee841a93864d1146a00c83bcfc16f5b49
Qingqi Shi <hi@qingqi.dev>
2025-03-04 23:19:49 +0000

    feat: add trailers (#1083)
    * feat: add trailers
    
    * tweak backdrop

--- src/app/[locale]/movie-database/[id]/page.tsx
+++ src/app/[locale]/movie-database/[id]/page.tsx
@@ -2,9 +2,10 @@ import * as stylex from "@stylexjs/stylex";
 import { Suspense } from "react";
 import { breakpoints } from "@/breakpoints";
 import { BackdropImage } from "@/components/movie-database/backdrop-image";
+import { TrailerButton } from "@/components/movie-database/trailer-button";
 import { border, color, font, space } from "@/tokens.stylex";
 import { getTranslations } from "@/utils/get-translations";
-import { fetchMovieDetails } from "@/utils/tmdb-api";
+import { fetchMovieDetails, fetchMovieVideos } from "@/utils/tmdb-api";
 import translations from "./translations.json";
 import type { PageProps } from "./types";
 
@@ -12,11 +13,18 @@ export default async function Page({ params }: PageProps) {
   const { id, locale } = await params;
   const { t } = getTranslations(translations, locale);
 
-  const movie = await fetchMovieDetails(id, { language: locale });
+  const [movie, videos] = await Promise.all([
+    fetchMovieDetails(id, { language: locale }),
+    fetchMovieVideos(id),
+  ]);
   const hours = Math.floor(movie.runtime / 60);
   const minutes = movie.runtime % 60;
 
   const formatter = new Intl.NumberFormat(locale, { maximumFractionDigits: 1 });
+
+  const trailer = videos.results?.find(
+    (video) => video.type === "Trailer" && video.official
+  );
 
   return (
     <>
@@ -52,6 +60,14 @@ export default async function Page({ params }: PageProps) {
         {(movie.overview || movie.tagline) && (
           <p css={styles.description}>{movie.overview ?? movie.tagline}</p>
         )}
+
+        <div css={styles.horizontalContainer}>
+          {!!trailer?.key && (
+            <TrailerButton trailerId={trailer.key} locale={locale}>
+              {t("playTrailer")}
+            </TrailerButton>
+          )}
+        </div>
       </div>
     </>
   );
@@ -80,6 +96,12 @@ const styles = stylex.create({
   description: {
     fontSize: font.size_1,
     margin: 0,
+  },
+  horizontalContainer: {
+    display: "flex",
+    alignItems: "center",
+    gap: space._1,
+    paddingTop: space._5,
   },
   ratingContainer: {
     width: space._9,
---
+++ src/components/shared/overlay.tsx
@@ -0,0 +1,94 @@
+import { X } from "@phosphor-icons/react/X";
+import * as stylex from "@stylexjs/stylex";
+
+import type { PropsWithChildren } from "react";
+import { breakpoints } from "@/breakpoints";
+import { useCssId } from "@/hooks/use-css-id";
+import { border, color, layer, space } from "@/tokens.stylex";
+import { Button } from "./button";
+
+interface OverlayProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function Overlay({
+  children,
+  isOpen,
+  onClose,
+}: PropsWithChildren<OverlayProps>) {
+  const id = useCssId();
+
+  return (
+    <>
+      {/* Using inline style until the new viewTransitionClass API is ready https://github.com/facebook/stylex/issues/866 */}
+      <style>
+        {`
+          ::view-transition-new(${id}-overlay) {
+            animation-name: ${slideIn};
+          }
+          ::view-transition-old(${id}-overlay) {
+            animation-name: ${slideOut};
+          }
+      `}
+      </style>
+      {isOpen && (
+        <div>
+          <div
+            css={styles.backdrop}
+            onClick={onClose}
+            style={{ viewTransitionName: `${id}-backdrop` }}
+          />
+          <div
+            css={styles.content}
+            style={{ viewTransitionName: `${id}-overlay` }}
+          >
+            <Button css={styles.closeButton} icon={<X />} onClick={onClose} />
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+const slideIn = stylex.keyframes({
+  from: {
+    transform: "translateY(100dvh)",
+  },
+});
+
+const slideOut = stylex.keyframes({
+  to: {
+    transform: "translateY(100dvh)",
+  },
+});
+
+const styles = stylex.create({
+  backdrop: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100dvw",
+    height: "100dvh",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    zIndex: layer.tooltip,
+  },
+  closeButton: {
+    position: "absolute",
+    right: { default: space._2, [breakpoints.md]: space._5 },
+    top: { default: space._2, [breakpoints.md]: space._5 },
+  },
+  content: {
+    position: "fixed",
+    top: space._8,
+    left: 0,
+    right: 0,
+    height: "100dvh",
+    backgroundColor: color.backgroundRaised,
+    paddingBottom: space._8,
+    zIndex: layer.tooltip,
+    borderRadius: border.radius_4,
+    overflow: "hidden",
+  },
+});
